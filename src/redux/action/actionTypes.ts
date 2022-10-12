38e231e06c4c85bd3dfec1145b6abee3790780cc
Nguyen Vu <nguyenvu191000@gmail.com>
2022-10-12 11:25:17 +0700

    update

---
+++ src/redux/action/actionTypes.ts
@@ -0,0 +1,9 @@
+export function typedAction<T extends string>(type: T): { type: T };
+export function typedAction<T extends string, P extends any>(
+  type: T,
+  payload: P
+): { type: T; payload: P };
+
+export function typedAction(type: String, payload?: any) {
+  return { type, payload };
+}
